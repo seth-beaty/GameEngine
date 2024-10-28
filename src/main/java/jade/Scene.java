33e41a8eca211c4a5bbac982954261ee0155b877
Seth Beaty <seth.a.beaty@gmail.com>
2024-10-28 15:30:33 -0400

    Added Renderer - push before LevelEditor Refactor

--- src/main/java/jade/Scene.java
+++ src/main/java/jade/Scene.java
@@ -1,10 +1,13 @@
 package jade;
 
+import renderer.Renderer;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Scene {
 
+    private Renderer renderer;
     protected Camera camera;
     private boolean isRunning = false;
     protected List<GameObject> gameObjects = new ArrayList<>();
@@ -20,6 +23,7 @@ public abstract class Scene {
     public void start() {
         for (GameObject go : gameObjects) {
             go.start();
+            this.renderer.add(go);
         }
         isRunning = true;
     }
@@ -29,8 +33,13 @@ public abstract class Scene {
         } else {
             gameObjects.add(go);
             go.start();
+            this.renderer.add(go);
         }
     }
 
     public abstract void update(float dt);
+
+    public Camera camera() {
+        return this.camera;
+    }
 }
