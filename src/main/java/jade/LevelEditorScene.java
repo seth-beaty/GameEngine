041e9c13e78b61df15cc74159eb4f8b5343c5fa4
Seth Beaty <seth.a.beaty@gmail.com>
2024-10-29 13:02:53 -0400

    Implemented AssetPool for shaders and Textures

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -3,6 +3,7 @@ package jade;
 import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
+import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
 
@@ -38,7 +39,11 @@ public class LevelEditorScene extends Scene {
                 this.addGameObjectToScene(go);
             }
         }
+        loadResources();
+    }
 
+    private void loadResources() {
+        AssetPool.getShader("assets/shaders/default.glsl");
     }
 
     @Override
---
+++ src/main/java/util/AssetPool.java
@@ -0,0 +1,38 @@
+package util;
+
+import renderer.Shader;
+import renderer.Texture;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AssetPool {
+
+    private static Map<String, Shader> shaders = new HashMap<>();
+    private static Map<String, Texture> textures = new HashMap<>();
+
+    public static Shader getShader(String resourceName) {
+        // Assuming passing full filepath relative to root
+        File file = new File(resourceName);
+        if (AssetPool.shaders.containsKey(file.getAbsolutePath())) {
+            return AssetPool.shaders.get(file.getAbsolutePath());
+        } else {
+            Shader shader = new Shader(resourceName);
+            shader.compileAndLink();
+            AssetPool.shaders.put(file.getAbsolutePath(), shader);
+            return shader;
+        }
+    }
+
+    public static Texture getTexture(String resourceName) {
+        File file = new File(resourceName);
+        if (AssetPool.textures.containsKey(file.getAbsolutePath())) {
+            return AssetPool.textures.get(file.getAbsolutePath());
+        } else {
+            Texture texture = new Texture(resourceName);
+            AssetPool.textures.put(file.getAbsolutePath(), texture);
+            return texture;
+        }
+    }
+}
