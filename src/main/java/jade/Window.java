81af5b0c2905b14b25a8432d02abf37ba4db8f91
Seth Beaty <seth.a.beaty@gmail.com>
2024-10-29 16:53:55 -0400

    Implemented texture batching

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -2,10 +2,8 @@ package jade;
 
 
 import org.lwjgl.Version;
-import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -128,7 +126,7 @@ public class Window {
     }
 
     public void loop() {
-        float beginTime = Time.getTime();
+        float beginTime = (float) glfwGetTime();
         float endTime;
         float dt = -1.0f;
 
@@ -148,7 +146,7 @@ public class Window {
 
             glfwSwapBuffers(glfwWindow);
 
-            endTime = Time.getTime();
+            endTime = (float) glfwGetTime();
             dt = endTime - beginTime;
             beginTime = endTime;
         }
